284bde757d8510cd43388a6c15b1860a1e387327
Sean-Peterson05 <Smpeterson241@gmail.com>
2022-09-19 15:41:12 -0400

    starting dropdown logic

--- assets/scripts/load_national_parks_data.js
+++ assets/scripts/load_national_parks_data.js
@@ -4,6 +4,7 @@ let locationsArray = [];
 let nationalParksArray = [];
 let parkTypesArray = [];
 let natParksbtn = document.querySelector("#nat-parks-btn");
+let parkTypebtn = document.querySelector("#byParktype")
 
 window.onload = function(){
 
@@ -29,9 +30,6 @@ async function loadJsonData(path) {
 
 natParksbtn.addEventListener("click", function() {
 
-    console.log(nationalParksArray)
-    console.log(nationalParksArray.length)
-
     let content = '';
 
     nationalParksArray.forEach(p => {
@@ -42,7 +40,7 @@ natParksbtn.addEventListener("click", function() {
         <div class="card">
         <div class="card-body">
           <h5 class="card-title">${p.LocationName}</h5>
-          <p class="card-text">${ p.Address}</p>
+          <p class="card-text">${p.Address}</p>
           <p class="card-text">${location}</p>
           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
@@ -53,5 +51,27 @@ natParksbtn.addEventListener("click", function() {
       document.querySelector("#shop").innerHTML = content;
 })
 
+parkTypebtn.addEventListener("click", function() {
 
+    // display all the park types to display 
+
+    // starter html that is static 
+    // dynamically add in html for the options 
+    // close with html that is static 
+    let content2 = `<select class="form-select" aria-label="Default select example">
+                    <option selected>Open this select menu</option>';
+                    `
+    let i = 1; 
+    parkTypesArray.forEach(p => {
+
+        content2 += `<option value=${i}>${p}</option>`;
+        i += 1;
+    })
+
+    content2 += '</select>'
+    console.log(content2)
+
+    document.querySelector("#dropdown").innerHTML = content2;
+
+})
 
