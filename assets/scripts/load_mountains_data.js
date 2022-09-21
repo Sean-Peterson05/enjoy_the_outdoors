b41dc2ebda8ce0c80a3a18d6be78de3cdb3f9153
Sean-Peterson05 <Smpeterson241@gmail.com>
2022-09-21 17:58:26 -0400

    Added view all mountains feature

--- assets/scripts/load_mountains_data.js
+++ assets/scripts/load_mountains_data.js
@@ -1,14 +1,16 @@
 "use strict"
 
 let mountainsArray = []
+let viewAllmtnsbtn = document.querySelector('#all-mtns-btn');
+let mtnsDropdown = document.querySelector('#mts-drop');
+let showMebtn = document.querySelector('#show-me-btn');
 
-window.onload = function(){
+
+window.onload = function () {
 
     loadJsonData("assets/data/mountains.json").then((mountains) => {
         mountainsArray = mountains.mountains;
     })
-
-    console.log(mountainsArray)
 }
 
 //function that can "fetch" the sunset/sunrise times
@@ -17,3 +19,81 @@ let loadJsonData = async (path) => {
     let data = await response.json()
     return data
 }
+
+// Create view all mountains function from mountain button - feed mountain details into html 
+viewAllmtnsbtn.addEventListener("click", function () {
+
+    //console.log(viewAllmtns);
+
+    let content = '';
+    mountainsArray.forEach(p => {
+
+        let imageSrc = "assets/images/mountains/" + p.img;
+        content +=
+            `<div style="max-width: 18rem;">
+          <img class="card-img-top" src="${imageSrc}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${p.name}</h5>
+            <p class="card-text">${p.desc}</p>
+            <p class="card-text">Intensity: ${p.effort}</p>
+            <p class="card-text">Elevation: ${p.elevation} feet </p>
+          </div>
+        </div>
+        `
+    });
+    document.querySelector("#shop2").innerHTML = content;
+});
+
+
+// Populate Mountain dropdown with mountains array 
+mtnsDropdown.addEventListener('click', function () {
+
+    console.log("Mountains Drop Down Populated")
+    let content4 = `<select class="form-select" id="mtn-select" aria-label="Default select example">
+                    <option selected>Select a Mountain</option>';
+                    `;
+    let i = 1;
+    mountainsArray.forEach(p => {
+
+        content4 += `<option value=${i}>${p.name}</option>`;
+        i += 1;
+    });
+
+    content4 += '</select>';
+
+    document.querySelector("#mts-drop").innerHTML = content4;
+}, {once : true})
+
+
+// Create function that filters cards by mountain selected 
+// Need to create a button that executes the below function 
+showMebtn.addEventListener('click', () => {
+
+
+        console.log("filter function ran");
+        let content = '';
+        let mtn_select = document.getElementById('mtn-select');
+        let mtn_select_text = mtn_select.options[mtn_select.selectedIndex].text;
+        console.log(mtn_select_text);
+        mountainsArray.forEach(p => {
+
+            if (p.Name === mtn_select_text) {
+
+                let imageSrc = "assets/images/mountains/" + p.img;
+                content +=
+                    `<div style="max-width: 18rem;">
+              <img class="card-img-top" src="${imageSrc}" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">${p.name}</h5>
+                <p class="card-text">${p.desc}</p>
+                <p class="card-text">Intensity: ${p.effort}</p>
+                <p class="card-text">Elevation: ${p.elevation} feet </p>
+              </div>
+            </div>
+            `;
+            }
+            document.querySelector("#shop2").innerHTML = content;
+            console.log(content);
+        });
+    }
+)
--- assets/scripts/load_national_parks_data.js
+++ assets/scripts/load_national_parks_data.js
@@ -111,7 +111,7 @@ function locationFilter() {
     })
 }
 
-// When location is selected, load results 
+// When park type is selected, load results 
 function typeFilter() {
     console.log("Function was called")
 
@@ -149,6 +149,10 @@ function typeFilter() {
 // Load all national parks 
 natParksbtn.addEventListener("click", function() {
 
+    // clear content from other button 
+    document.querySelector("#loc-dropdown").innerHTML = '';
+    document.querySelector("#ptype-dropdown").innerHTML = '';
+
     let content = '';
     nationalParksArray.forEach(p => {
 
@@ -174,171 +178,6 @@ natParksbtn.addEventListener("click", function() {
     document.querySelector("#shop").innerHTML = content;
 }); 
 
-
-    
-    // Get the value of the location selected by user 
-//     let location_select = document.getElementById('location-select');
-//     let park_type_select = document.getElementById('park_type-select');
-    
-//     // Build content html dynamically 
-
-//     // if location and park type are not null then filter for both 
-//     if(location_select !== null && park_type_select !== null){
-
-//         let location_select_text = location_select.options[location_select.selectedIndex].text;
-//         let park_type_text = park_type_select.options[park_type_select.selectedIndex].text;
-
-//         let content = '';
-//         nationalParksArray.forEach(p => {
-
-//             if(p.State === location_select_text && p.LocationName.includes(park_type_text)){
-     
-//                  let location = p.City + ", " + p.State;
-     
-//                  content += `
-//                  <div class="card">
-//                  <div class="card-body">
-//                  <h5 class="card-title">${p.LocationName}</h5>
-//                  <p class="card-text">${p.Address}</p>
-//                  <p class="card-text">${location}</p>
-//                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//                  </div>
-//              </div>
-//                  `
-//         document.querySelector("#shop").innerHTML = content;
-     
-//      }})
-//     }
-//     // if location is selected and park type is null then filter for location 
-//     else if(location_select !== null && park_type_select === null){
-
-//         let location_select_text = location_select.options[location_select.selectedIndex].text;
-
-//         let content = '';
-//         nationalParksArray.forEach(p => {
-     
-//             if(p.State === location_select_text){
-//                  let location = p.City + ", " + p.State;
-     
-//                  content += `
-//                  <div class="card">
-//                  <div class="card-body">
-//                  <h5 class="card-title">${p.LocationName}</h5>
-//                  <p class="card-text">${p.Address}</p>
-//                  <p class="card-text">${location}</p>
-//                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//                  </div>
-//              </div>
-//           `
-//             }
-//         document.querySelector("#shop").innerHTML = content;
-     
-//      })
-//     }
-//     // If location is null then filter for park type
-//     else if(location_select === null && park_type_select !== null){
-
-//         let park_type_text = park_type_select.options[park_type_select.selectedIndex].text;
-
-//         let content = '';
-//         nationalParksArray.forEach(p => {
-     
-//             if(p.LocationName.includes(park_type_text)){
-//                  let location = p.City + ", " + p.State;
-     
-//                  content += `
-//                  <div class="card">
-//                  <div class="card-body">
-//                  <h5 class="card-title">${p.LocationName}</h5>
-//                  <p class="card-text">${p.Address}</p>
-//                  <p class="card-text">${location}</p>
-//                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//                  </div>
-//              </div>
-//           `
-//             }
-//         document.querySelector("#shop").innerHTML = content;
-     
-//      })
-//     }
-
-//     // Show all National Parks by Default
-//     else{
-
-//         let content = '';
-//         nationalParksArray.forEach(p => {
-     
-            
-//                  let location = p.City + ", " + p.State;
-     
-//                  content += `
-//                  <div class="card">
-//                  <div class="card-body">
-//                  <h5 class="card-title">${p.LocationName}</h5>
-//                  <p class="card-text">${p.Address}</p>
-//                  <p class="card-text">${location}</p>
-//                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//                  </div>
-//              </div>
-//           `
-            
-//         document.querySelector("#shop").innerHTML = content;
-     
-//      })
-//     }
-
-// })
-     
-    // if(location_select !== null) {
-    //     let location_select_text = location_select.options[location_select.selectedIndex].text;
-    //     console.log(location_select_text);
-    // }
-    // else {
-    //     let location_select_text = "All States";
-    //     console.log(location_select_text);
-
-    // }
-    // //console.log(location_select_text);
-    
-    // // Get the value of the park type selected by user 
-    // let park_type_select = document.getElementById('park_type-select');
-
-    // if(park_type_select !== null){
-    //     let park_type_text = park_type_select.options[park_type_select.selectedIndex].text;
-    //     console.log(park_type_text);
-    // }
-    // else{
-    //     let park_type_text = "All Types";
-    //     console.log(park_type_text);
-    // }
-
-    // Build content html dynamically 
-    // if only one filter is selected then filter for just the one
-    // if both filters are selected then filter for both 
-    // if no filter is selected then show all results 
-    //p.LocationName.includes(park_type_text)
-//     let content = '';
-
-//     nationalParksArray.forEach(p => {
-        
-//        // if(location_select_text === "All States" || park_type_text === "All Types") { 
-
-//             let location = p.City + ", " + p.State;
-
-//             content += `
-//             <div class="card">
-//             <div class="card-body">
-//             <h5 class="card-title">${p.LocationName}</h5>
-//             <p class="card-text">${p.Address}</p>
-//             <p class="card-text">${location}</p>
-//             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//             </div>
-//         </div>
-//             `
-//             document.querySelector("#shop").innerHTML = content;
-//        // }
-
-// });
 
 
 
